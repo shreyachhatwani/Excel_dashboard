674cf8bad9a621e054062e29b285e270cb0be94c
shreyachhatwani <chhatwanishreya@gmail.com>
2026-06-24 12:35:16 +0530

    env creation

--- config.py
+++ config.py
@@ -1,17 +1,22 @@
-# config.py
+import os
+from dotenv import load_dotenv
+
+# This looks for the .env file and loads its variables into your system memory
+load_dotenv()
 
 # ── Neo4j AuraDB cloud credentials (from console.neo4j.io)
-NEO4J_URI      = "neo4j+s://f0629c74.databases.neo4j.io"
-NEO4J_USER     = "neo4j"
-NEO4J_PASSWORD = "Z0KaSjUb8rKCdFECzV-onQ7E0XRrZMLzrrpABrHSIrs"
+NEO4J_URI  = "neo4j+s://f0629c74.databases.neo4j.io"
+NEO4J_USER = "neo4j"
+# Fetched securely from your local .env file
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
 
 # ── PostgreSQL local credentials
-# These are the values you set during PostgreSQL installation
 POSTGRES_HOST     = "127.0.0.1"
 POSTGRES_PORT     = 5433
 POSTGRES_USER     = "postgres"
-POSTGRES_PASSWORD = "87654321@"   # what you set during install
-POSTGRES_DB       = "dashboard_db"               # the DB you created in pgAdmin
+# Fetched securely from your local .env file
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB       = "dashboard_db"
 
 # ── Input source — set ONE
 EXCEL_FILE_PATH   = r""
